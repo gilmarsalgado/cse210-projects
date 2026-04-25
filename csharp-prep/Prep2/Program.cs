63f94edbc90b6a736c816a2ab34aacb6989d2957
gilmarsalgadojr <gilmarsalgadojr@hotmail.com>
2026-04-25 12:59:49 -0700

    calculating grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string final = "";
+
+        if (percent >= 90)
+        {
+            final = "A";
+        }
+        else if (percent >= 80)
+        {
+            final = "B";
+        }
+        else if (percent >= 70)
+        {
+            final = "C";
+        }
+        else if (percent >= 60)
+        {
+            final = "D";
+        }
+        else
+        {
+            final = "F";
+        }
+
+        Console.WriteLine($"Your final grade is: {final}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do it!");
+        }
     }
 }
