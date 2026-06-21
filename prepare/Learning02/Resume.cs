b270a12c14160c779c35f59aa49f3c9aa0be501d
gilmarsalgadojr <gilmarsalgadojr@hotmail.com>
2026-06-20 23:14:29 -0700

    Learning02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Resume{
+    public string name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void DisplayDetails(){
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine("Jobs: ");
+
+        foreach(Job job in _jobs)
+        {
+            job.DisplayJobDetails();
+        }
+    }
+}
