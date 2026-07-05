e152ad5978da80dc73b594872964a321e45552cb
gilmarsalgadojr <gilmarsalgadojr@hotmail.com>
2026-07-04 23:27:55 -0600

    Completing assignment 4

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,71 @@
+using System;
+
+class Activity{
+    private string _name;
+    private string _description;
+    protected int _duration;
+
+    public Activity(string name, string description){
+        _name = name;
+        _description = description;
+
+    }
+
+    public void DisplayStartingMessage(){
+        Console.WriteLine($"Welcome to the {_name}.");
+        Console.WriteLine("");
+        Console.WriteLine(_description);
+        Console.Write($"\nHow Long, in seconds, would you like for your session? ");
+    }
+    public void DisplayEndingMessage(){
+        Console.WriteLine("\nWell done!");
+        ShowSpinner(3);
+        Console.WriteLine($"\nYou have completed another {_duration} seconds of the {_name}");
+        ShowSpinner(5);
+        Console.Clear();
+    }
+    public void ShowSpinner(int seconds){
+        List<string> slash = new List<string>();
+        slash.Add("|");
+        slash.Add("/");
+        slash.Add("-");
+        slash.Add("\\");
+        slash.Add("|");
+        slash.Add("/");
+        slash.Add("-");
+        slash.Add("\\");
+
+        DateTime _dateTime = DateTime.Now;
+        DateTime _endTime = _dateTime.AddSeconds(seconds);
+        int y = 0;
+        while(DateTime.Now <_endTime){
+            string s = slash[y];
+            Console.Write(s);
+            Thread.Sleep(500);
+            Console.Write("\b \b");
+            y++;
+            if(y >= slash.Count){
+                y = 0;
+            }
+        }
+    }
+    public void ShowCountDown(int seconds){
+        DateTime _dateTime = DateTime.Now;
+        DateTime _endTime = _dateTime.AddSeconds(seconds);
+        int y = seconds;
+        while(DateTime.Now <_endTime){
+            Console.Write(y);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+            y--;
+        }
+    }
+
+    public void GetReady(int seconds){
+        Console.WriteLine("Get Ready...");
+        ShowSpinner(seconds);
+        Console.WriteLine("");
+    }
+
+
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,48 @@
 using System;
 
+// Exceed Requirements
+// Dictionary for count each activity that the user made.
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Dictionary<string,int> countActivity = new Dictionary<string,int>{
+            {"Breathing", 0},
+            {"Reflecting", 0},
+            {"Listening", 0}
+        };
+        int s = 0;
+        while (s != 4){
+            Console.WriteLine("\nMenu options:");
+            Console.WriteLine(" 1. Start breathing activity");
+            Console.WriteLine(" 2. Start reflecting activity");
+            Console.WriteLine(" 3. Start Listening activity");
+            Console.WriteLine(" 4. Quit");
+            Console.Write("Select a choice from the menu: ");
+            s = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            if (s == 1){
+                BreathingActivity breathingActivity = new BreathingActivity();
+                breathingActivity.Run();
+                countActivity["Breathing"]++;
+            } else if(s == 2){
+                ReflectingActivity reflecting = new ReflectingActivity();
+                reflecting.Run();
+                countActivity["Reflecting"]++;
+            } else if(s == 3){
+                ListingActivity listing = new ListingActivity();
+                listing.Run();
+                countActivity["Listening"]++;
+            } else if(s == 4){
+                break;
+            }
+        }
+        Console.Clear();
+        Console.WriteLine("Congratulations!");
+        Console.WriteLine($"{countActivity["Breathing"]} Breathing Activity");
+        Console.WriteLine($"{countActivity["Reflecting"]} Reflecting Activity");
+        Console.WriteLine($"{countActivity["Listening"]} Listening Activity");
     }
 }
